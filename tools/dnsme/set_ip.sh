9908d5e0b8a4a8f1913c5e0270b51307191a1dab
Janakiram MSV <janakiramm@users.noreply.github.com>
2016-11-21 04:39:43 +0530

    Update set_ip.sh

--- tools/dnsme/set_ip.sh
+++ tools/dnsme/set_ip.sh
@@ -1,3 +1,6 @@
 export INT_IP=`ifconfig eth1 | grep "inet " | awk -F'[: ]+' '{ print $4 }'`
-perl /opt/cloudshop/tools/dnsme/dnsmeapi.pl https://api.dnsmadeeasy.com/V2.0/dns/managed/4451671/records/39799308 -X PUT -H accept:application/json -H content-type:application/json -d '{"name":"'$1'","type":"A","value":"'$INT_IP'","id":"'$2'","gtdLocation":"DEFAULT","ttl":30}'
-#echo "@reboot root sh /opt/cloudshop/tools/dnsme/set_ip.sh web1.int 39799308" >> /etc/crontab
+perl /opt/cloudshop/tools/dnsme/dnsmeapi.pl "https://api.dnsmadeeasy.com/V2.0/dns/managed/$2/records/$3" -X PUT -H accept:application/json -H content-type:application/json -d '{"name":"'$1'","type":"A","value":"'$INT_IP'","id":"'$3'","gtdLocation":"DEFAULT","ttl":30}'
+
+# perl ./dnsmeapi.pl https://api.dnsmadeeasy.com/V2.0/dns/managed/
+# perl ./dnsmeapi.pl https://api.dnsmadeeasy.com/V2.0/dns/managed/<DOM_ID>/records
+#echo "@reboot root sh /opt/cloudshop/tools/dnsme/set_ip.sh web1.int 4726991 41894386" >> /etc/crontab
